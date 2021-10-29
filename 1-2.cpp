c1f07d47a7b4d7e32d014f77964e8ef1853418ab
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-29 18:29:04 +0300

    Исходный код

---
+++ 1-2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	float R, V, S;
+	
+	cout << "Vvedi R: ";
+	cin >> R;
+	
+	V = (4/3)*(3.14)*R*R*R;
+	S = 4*(3.14)*R*R;
+	
+	cout << "V shara = " << V << endl;
+	cout << "S shara = " << S << endl;
+	return 0;
+}
+
