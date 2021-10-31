8dbf5412f2eeef75fd266a75bf8b54d5d30813fa
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-31 12:27:19 +0300

    Update 1-3.cpp

--- 1-3.cpp
+++ 1-3.cpp
@@ -6,9 +6,12 @@ int main(){
 	
 	cout << "Vvedi R1, R2, R3: ";
 	cin >> R1 >> R2 >> R3;
-	
+	if (!R1 || !R2 || !R3) {
+		cout << "Vvodi chisla! " << endl;
+	}
+	else {
 	R = 1/((1/R1) + (1/R2) + (1/R3));
-	
 	cout << "R cepi = " << R << endl;
+	}
 	return 0;
 }
