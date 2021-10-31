beb1129d8c0c7eea3f37087b5362a63c984393bb
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-31 11:06:41 +0300

    Исходный код задания 3-2 б

---
+++ 3-2b.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double sum = 0; double sume = 0; double res = 1;
+	float n = 0; float a,b,e;
+	cout << "Vvedi n: ";
+	cin >> n; cout << endl;
+	cout << "Vvedi s kakogo poroga zapisivaem v summu : ";
+	cin >> e; cout << endl;
+	
+	for (int i = 0; i < n; i++){
+		res = 1;
+		for (int u = 1; u <= i; u++) {
+			res = res * u;
+  		}
+  		
+		a = pow((-1),i);
+		sume = a/res;
+		
+		if (sume >= e){
+			sum = sum + sume;
+		}
+		
+	}
+	
+	cout << "Summa ryada pri Sn > "<< e << " = " << sum << endl;
+
+	return 0;
+}
+
