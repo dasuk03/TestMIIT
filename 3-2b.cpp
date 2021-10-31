5c34b58c4f2ad90cb5d52379231b1e1d5553bb5e
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-31 11:55:23 +0300

    Update 3-2b.cpp

--- 3-2b.cpp
+++ 3-2b.cpp
@@ -10,23 +10,24 @@ int main(){
 	cout << "Vvedi s kakogo poroga zapisivaem v summu : ";
 	cin >> e; cout << endl;
 	
-	for (int i = 0; i < n; i++){
-		res = 1;
-		for (int u = 1; u <= i; u++) {
-			res = res * u;
-  		}
-  		
-		a = pow((-1),i);
-		sume = a/res;
-		
-		if (sume >= e){
-			sum = sum + sume;
+	if (n && e) {
+		for (int i = 0; i < n; i++){
+			res = 1;
+			for (int u = 1; u <= i; u++) {
+				res = res * u;
+	  		}
+	  		
+			a = pow((-1),i);
+			sume = a/res;
+			
+			if (sume >= e){
+				sum = sum + sume;
+			}
 		}
-		
+		cout << "Summa ryada pri Sn > "<< e << " = " << sum << endl;
 	}
-	
-	cout << "Summa ryada pri Sn > "<< e << " = " << sum << endl;
-
+	else {
+		cout << " Eto ne chislo! " << endl;
+	}
 	return 0;
 }
-
