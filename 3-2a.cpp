31271af4a555c110336ab64df52f8a26f0e8cb0e
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-31 11:53:01 +0300

    Update 3-2a.cpp

--- 3-2a.cpp
+++ 3-2a.cpp
@@ -2,28 +2,29 @@
 #include <cmath>
 using namespace std;
 
-
 int main(){
 	double sum = 0; double res = 1;
 	float n = 0; float a,b;
 	cout << "Vvedi n: ";
 	cin >> n; cout << endl;
 	
-	// n = 1 - ñêîëüêî ýë âñåãî
-	
-	for (int i = 0; i < n; i++){
-		res = 1;
-		for (int u = 1; u <= i; u++) {
-			res = res * u;
-  		}
-		a = pow((-1),i);
-		cout << " a = " << a << endl;
-		cout << " res = " << res << endl;
-		sum = sum + (a/res);
-		cout << " sum = " << sum << endl;
+	// n = 1 - Ã±ÃªÃ®Ã«Ã¼ÃªÃ® Ã½Ã« Ã¢Ã±Ã¥Ã£Ã®
+	if (n) {
+		for (int i = 0; i < n; i++){
+			res = 1;
+			for (int u = 1; u <= i; u++) {
+				res = res * u;
+	  		}
+			a = pow((-1),i);
+			cout << " a = " << a << endl;
+			cout << " res = " << res << endl;
+			sum = sum + (a/res);
+			cout << " sum = " << sum << endl;
+		}
+		cout << sum;
 	}
-	
-	cout << sum;
-
+	else {
+		cout << "Ne chislo! " << endl;
+	}
 	return 0;
 }
