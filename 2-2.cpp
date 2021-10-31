82547972451d3ae2561f6a92cc3ae15a4b51bd42
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-10-31 12:19:16 +0300

    Update 2-2.cpp

--- 2-2.cpp
+++ 2-2.cpp
@@ -9,17 +9,21 @@ int main(){
 	
 	cout << "Vvedi x: ";
 	cin >> x;
-	
-	cout << "a = " << a << endl;
-	cout << "x = " << x << endl;
-	if (x < 1.2) {
-		y = a*x*x+4;
-		cout << "x < 1.2" << endl;
+	if (!x) {
+		cout << "Vvedi chislo! " << endl;
 	}
 	else {
-		y = (a + 4*x)*sqrt(pow(x,2*a));
-		cout << "x >= 1.2" << endl;
+		cout << "a = " << a << endl;
+		cout << "x = " << x << endl;
+		if (x < 1.2) {
+			y = a*x*x+4;
+			cout << "x < 1.2" << endl;
+		}
+		else {
+			y = (a + 4*x)*sqrt(pow(x,2*a));
+			cout << "x >= 1.2" << endl;
+		}
+		cout << "y = " << y << endl;
 	}
-	cout << "y = " << y << endl;
 	return 0;
 }
