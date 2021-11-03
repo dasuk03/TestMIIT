df8bbaa15c32b3bb1d622d58bb945256556b9c30
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-11-03 09:41:07 +0300

    Update 1-3.cpp

--- 1-3.cpp
+++ 1-3.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
 
+float R(float R1, float R2, float R3) {
+	return 1/((1/R1) + (1/R2) + (1/R3));
+	/*
+	return возвращает значение Сопротивления цепи
+	*/
+}
+
 int main(){
-	float R1, R2, R3, R;
+	float R1, R2, R3;
 	
-	cout << "Vvedi R1, R2, R3: ";
+	setlocale( LC_ALL,"Russian" ); // Правильный вывод Кириллицы 
+	
+	cout << "Введи сопротивления R1, R2, R3: ";
+	
 	cin >> R1 >> R2 >> R3;
+	
 	if (!R1 || !R2 || !R3) {
-		cout << "Vvodi chisla! " << endl;
+		cout << "Надо вводить числа! " << endl;
 	}
 	else {
-	R = 1/((1/R1) + (1/R2) + (1/R3));
-	cout << "R cepi = " << R << endl;
+	cout << "Сопротивление цепи = " << R(R1, R2, R3) << endl;
 	}
-	return 0;
+	
+	return 0; // Успешное завершение кода
 }
