9777f2aebfbede938cedcdb5c404c8b54728f0d6
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-11-03 11:10:21 +0300

    Update 3-2a.cpp

--- 3-2a.cpp
+++ 3-2a.cpp
@@ -2,29 +2,52 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-	double sum = 0; double res = 1;
-	float n = 0; float a,b;
-	cout << "Vvedi n: ";
+double faktorial(int i){ // факториал числа
+	
+	double res = 1; // начально значение факториала !0 = 1
+	for (int u = 1; u <= i; u++) {
+		res = res * u;
+	}
+	return res; // Возвращает значение факториала 
+	
+	/*
+	Функция faktorial принимает номер элемента факториала
+	и возвращает его значение
+	*/
+}
+
+double summa() {
+	
+	float a,b;
+	float n = 0; double sum = 0;
+	cout << "Введи n: ";
 	cin >> n; cout << endl;
 	
-	// n = 1 - сколько эл всего
 	if (!n) {
-		cout << "Ne chislo! " << endl;
+		cout << "Введи число! " << endl;
 	}
 	else {
+	
 		for (int i = 0; i < n; i++){
-			res = 1;
-			for (int u = 1; u <= i; u++) {
-				res = res * u;
-	  		}
-			a = pow((-1),i);
-			cout << " a = " << a << endl;
-			cout << " res = " << res << endl;
-			sum = sum + (a/res);
-			cout << " sum = " << sum << endl;
+				
+			// faktorial(i); // Вызов факториала i элемента
+			
+			a = pow((-1),i); // Степень (-1) 
+			
+			// cout << " a = " << a << endl;
+			// cout << " res = " << faktorial(i) << endl;
+			
+			sum = sum + (a/faktorial(i)); // Счётчик суммы 
+			
 		}
-		cout << sum;
+		cout << "sum = " << sum << endl; // Вывод суммы 
 	}
-	return 0;
+}
+
+int main(){
+	
+	setlocale( LC_ALL,"Russian" ); // Вывод Кириллицы
+	summa(); // Вызов функции подсчёта суммы 
+	return 0; // Успешное завершение кода
+	
 }
