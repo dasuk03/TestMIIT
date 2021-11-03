a938829c6eb02b3ed7da5a52ecc9b91bf53cbe18
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2021-11-03 09:55:34 +0300

    Update 2-2.cpp

--- 2-2.cpp
+++ 2-2.cpp
@@ -2,28 +2,48 @@
 #include <cmath>
 using namespace std;
 
+float f1(float x){
+	float const a = 2.8;
+	return (a*x*x+4); // Возвращает значение функции
+	/*
+	Функция f1 вызывается при x < 1.2 и 
+	возвращает значение Функции (с такими Х)
+	*/
+}
+
+float f2(float x){
+	float const a = 2.8;
+	return ((a + 4*x)*sqrt(pow(x,2*a))); // Возвращает значение функции
+	/*
+	Функция f2 вызывается при x >= 1.2 и 
+	возвращает значение Функции (с такими Х)
+	*/
+}
+
 int main(){
-	float a, x, y;
+	float x;
+	setlocale( LC_ALL,"Russian" ); //Правильный вывод Кириллицы
 	
-	a = 2.8;
-	
-	cout << "Vvedi x: ";
+	cout << "Введи x: ";
 	cin >> x;
-	if (!x) {
-		cout << "Vvedi chislo! " << endl;
+	if(!x) { 	// if(!x) проверят формат ввода
+		
+		cout << "Надо вводить число! " << endl;
+		
 	}
 	else {
-		cout << "a = " << a << endl;
-		cout << "x = " << x << endl;
-		if (x < 1.2) {
-			y = a*x*x+4;
-			cout << "x < 1.2" << endl;
+											      
+		cout << "x = " << x << endl;			
+												
+		if (x < 1.2) {						   // Проверка Х для выбора Формулы
+			cout << "x < 1.2" << endl;        // Вывод условия выбора формулы решения 
+			cout << "y = " << f1(x) << endl; // Вывод значения Формулы при x > 1.2
 		}
 		else {
-			y = (a + 4*x)*sqrt(pow(x,2*a));
 			cout << "x >= 1.2" << endl;
+			cout << "y = " << f2(x) << endl;
 		}
-		cout << "y = " << y << endl;
+		
 	}
-	return 0;
+	return 0; // Успешное завершение Функции 
 }
